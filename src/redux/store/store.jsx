9625625e1e2c5dcfff395ca486c869591e1daff2
romeJG <58583590+romeJG@users.noreply.github.com>
2024-09-06 17:37:00 +0800

    remove counter

--- src/redux/store/store.jsx
+++ src/redux/store/store.jsx
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../slice/counterSlice';
 import todoReducer from '../slice/todoSlice';
 const store = configureStore({
     reducer: {
-        counter: counterReducer,
         toDoList: todoReducer,
     },
 });
